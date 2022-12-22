2c73a9e3dfa00ae10d6f987930ebde3efe05d8ed
Barhamou <hama.peinture.barhamou@gmail.com>
2022-12-22 10:47:57 +0100

    Optimisation du code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,11 +6,9 @@ def pascal_triangle(n):
         return [[1]]
     elif n == 2:
         return [[1], [1, 1]]
-    elif n == 3:
-        return [[1], [1, 1], [1, 2, 1]]
     else:
-        liste = [[1], [1, 1], [1, 2, 1]]
-        for __ in range(4, n+1, 1):
+        liste = [[1], [1, 1]]
+        for __ in range(3, n+1, 1):
             endliste = liste[len(liste) - 1]
             new_liste = [1]
             for i in range(1, len(endliste), 1):
