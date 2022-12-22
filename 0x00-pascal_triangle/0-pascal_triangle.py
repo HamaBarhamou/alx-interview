a012443378aa3c7c13861e65d8dcc04a67cb9fcf
Barhamou <hama.peinture.barhamou@gmail.com>
2022-12-22 10:45:17 +0100

    0. Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,9 +5,16 @@ def pascal_triangle(n):
     if n == 1:
         return [[1]]
     elif n == 2:
-        return [[1],[1, 1]]
+        return [[1], [1, 1]]
+    elif n == 3:
+        return [[1], [1, 1], [1, 2, 1]]
     else:
-        liste = [[1],[1, 1]]
-        l = [1, 1]
-        for __ in range(3, n+1, 1):
-            pass
+        liste = [[1], [1, 1], [1, 2, 1]]
+        for __ in range(4, n+1, 1):
+            endliste = liste[len(liste) - 1]
+            new_liste = [1]
+            for i in range(1, len(endliste), 1):
+                new_liste.append(endliste[i-1] + endliste[i])
+            new_liste.append(1)
+            liste.append(new_liste)
+        return liste
