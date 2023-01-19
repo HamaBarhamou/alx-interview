acc30bf582cee456b02a752bc577fe47fb2c51d0
HAMA Barhamou <hamabarhamou@gmail.com>
2023-01-19 12:16:04 +0100

    essai

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python3
+
+def read()->[bool, bool, str]:
+    try:
+        x = input()
+        return [True, False, x]
+    except KeyboardInterrupt:
+        return [True, True, "error"]
+
 rep = True
 cpt = 0
 somme_file_size = 0
+dic = {}
+rep = True
 while (rep):
-    try:
+    rep, state_err, x = read()
+    if state_err == False:
         x = input()
         cpt += 1
         somme_file_size += int(x.split(' ')[-1])
         if cpt == 10:
-            print ('File size: ', somme_file_size)
+            print('File size:', somme_file_size)
             cpt = 0
             somme_file_size = 0
+            for key, value in sorted(dic.items()):
+                print(key, ":", value)
+            dic = {}
         else:
-            print(x.split(" ")[-2])
-    except KeyboardInterrupt:
-        print ('File size:')
-        rep = False
+            key = x.split(" ")[-2]
+            if key in dic:
+                dic[key] += 1
+            else:
+                dic[key] = 1
+    else:
+        print("hello on continue...")
