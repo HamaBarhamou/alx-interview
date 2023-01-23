5d905d9ede16fcf03ff5279ca9659b3cf89fb47d
HAMA Barhamou <hamabarhamou@gmail.com>
2023-01-23 11:13:00 +0100

    initiation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+0. UTF-8 Validation
+"""
+
+
+def validUTF8(data):
+    """
+        A method that determines if a given data
+        set representsa valid UTF-8 encoding.
+    """
+    pass
