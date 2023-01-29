4e948bd266a57aadb1d00fe820320d562def579b
HAMA Barhamou <hamabarhamou@gmail.com>
2023-01-29 15:52:52 +0100

    0. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,12 +1,35 @@
 #!/usr/bin/python3
-"""
-0. UTF-8 Validation
-"""
+"""UTF-8 Validation"""
 
 
 def validUTF8(data):
     """
-        A method that determines if a given data
-        set representsa valid UTF-8 encoding.
+    Validates a set of characters represented by a list
+    of integers.
+
+    Must be UTF-8 Validated.
+    Every integer is taken as a byte represented in
+    8-bit binary number. The first byte in UTF-8
+    is called the significant byte and states the
+    number of bytes that the character will have.
     """
-    pass
+    num_bytes = 0
+    mask = 255
+
+    for byte in data:
+        if num_bytes == 0:
+            byte = byte & mask
+            if (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif (byte >> 7):
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
