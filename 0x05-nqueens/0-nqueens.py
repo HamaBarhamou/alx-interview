0365eb8564c0d2f3f1663d26d984b8871dbd7e5f
HAMA Barhamou <hamabarhamou@gmail.com>
2023-02-05 12:41:17 +0100

    0. N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,37 +1,43 @@
-#!/usr/bin/env python3
-"""
-    The N queens puzzle is the challenge of placing
-    N non-attacking queens on an N×N chessboard.
-"""
-import sys
+#!/usr/bin/python3
+'''
+Module that solves the N Queens puzzle
+'''
+from sys import argv, exit
 
-
-def main():
-    """
-    implements all possible solutions to
-    the n queens problem
-    """
-    if len(sys.argv) != 2:
+if __name__ == "__main__":
+    if len(argv) != 2:
         print('Usage: nqueens N')
-        sys.exit(1)
-
+        exit(1)
     try:
-        n = int(sys.argv[1])
-    except ValueError:
+        n = int(argv[1])
+    except BaseException:
         print('N must be a number')
-        sys.exit(1)
-
+        exit(1)
     if n < 4:
         print('N must be at least 4')
-        sys.exit(1)
+        exit(1)
 
-    aux = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    board = {}
-    for i in range(1, n + 1):
-        board[i] = aux[i - 1]
+    solution = []
 
-    return []
+    def solve_queens(row, n, solution):
+        if (row == n):
+            print(solution)
+        else:
+            for col in range(n):
+                placement = [row, col]
+                if valid_placement(solution, placement):
+                    solution.append(placement)
+                    solve_queens(row + 1, n, solution)
+                    solution.remove(placement)
 
+    def valid_placement(solution, placement):
+        for queen in solution:
+            if queen[1] == placement[1]:
+                return False
+            if (queen[0] + queen[1]) == (placement[0] + placement[1]):
+                return False
+            if (queen[0] - queen[1]) == (placement[0] - placement[1]):
+                return False
+        return True
 
-if __name__ == '__main__':
-    main()
+    solve_queens(0, n, solution)
