dab449467b03598e326d8d04e8a9e6448c0663b7
HAMA Barhamou <hamabarhamou@gmail.com>
2023-02-15 17:12:52 +0100

    initiation

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Test 0x07 - Rotate 2D Matrix
+"""
+
+def rotate_2d_matrix(matrix):
+    ligne = len(matrix)
+    colonne = len(matrix[0])
+    for line in matrix:
+        for col in line:
+            pass
